cce78ad7b33a89025d60831296cb1ed9e263d5e1
sebcarlucci <sebastiano.carlucci@gmail.com>
2018-01-25 23:40:48 -0800

    added comments

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -6,21 +5,23 @@ import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.Line2D;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
+
 
 @SuppressWarnings("serial")
 public class Main extends JFrame {
-
-	private static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	
+	// Set the dimensions of the frame
+	private static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	public static final int WIDTH = (int) screenSize.getWidth(), HEIGHT = (int) screenSize.getHeight();
+	
+	// Set the parameters for the slider
 	public static final int SLIDER_INIT = 250, SLIDER_MAX = 2 * SLIDER_INIT;
 	private JSlider angleSlider;
+	
+	// Set the parameters for the tree
 	private double rotAngle = Math.PI/2;
 	private boolean movingRight = true;
 	public static boolean animating = true;
-	private BufferedImage imgPlay = null;
 	
 	public Main(){
 		
@@ -40,12 +41,14 @@ public class Main extends JFrame {
 		//Create nested class of DrawCanvas
 		class DrawCanvas extends JPanel{
 			
-			public static final double INITIAL_LENGTH = 220;
+			public static final double INITIAL_LENGTH = 190;
 			
 			@Override
 			public void paintComponent(Graphics g){
 				super.paintComponent(g);
-				setBackground(new Color(34, 104, 145));
+				setBackground(new Color(45,45,45));
+				
+				// Draw the tree
 				g.setColor(Color.WHITE);
 				Graphics2D g2d = (Graphics2D) g;
 				drawBranch(g2d,cp.getWidth()/2.0,(double)cp.getHeight(), Math.PI/2, INITIAL_LENGTH, 20);
@@ -54,11 +57,13 @@ public class Main extends JFrame {
 			private void drawBranch(Graphics2D g2d, double x, double y, double angle,double length, float stroke) {
 				g2d.setStroke(new BasicStroke(stroke));
 				g2d.draw(new Line2D.Double(x, y, x+length*Math.cos(angle), y-length*Math.sin(angle)));
+				
+				// Stopping condition for the recursion, there is no need to keep going infinetly
 				if(length > 4){
 					drawBranch(g2d, x+length*Math.cos(angle), y-length*Math.sin(angle),angle-rotAngle*0.3, length*0.70, stroke*0.80f);
 					//drawBranch(g2d, x+length*Math.cos(angle), y-length*Math.sin(angle),angle-rotAngle, length*0.80, stroke*0.80f);
-					//drawBranch(g2d, x+length*Math.cos(angle), y-length*Math.sin(angle),angle+rotAngle, length*0.80, stroke*0.80f);
-					drawBranch(g2d, x+length*Math.cos(angle), y-length*Math.sin(angle),angle+(Math.PI/2-rotAngle), length*0.70, stroke*0.80f);
+					drawBranch(g2d, x+length*Math.cos(angle), y-length*Math.sin(angle),angle+rotAngle, length*0.80, stroke*0.80f);
+					//drawBranch(g2d, x+length*Math.cos(angle), y-length*Math.sin(angle),angle+(Math.PI/2-rotAngle), length*0.70, stroke*0.80f);
 
 				}
 			}
@@ -79,7 +84,6 @@ public class Main extends JFrame {
 		angleSlider.setPaintTicks(true);
 		angleSlider.setMajorTickSpacing(SLIDER_INIT/10);
 		angleSlider.setMinorTickSpacing(SLIDER_MAX/10);
-		angleSlider.setBackground(Color.BLACK);
 		angleSlider.setBounds(10, 10, 200, 50);
 		canvas.add(angleSlider);
 		angleSlider.setOpaque(false);
@@ -87,6 +91,8 @@ public class Main extends JFrame {
 		
 		
 		//Setting up the action listeners
+		
+		// Lets the user select the angle between each branch
 		angleSlider.addChangeListener(new ChangeListener(){
 
 			@Override
@@ -96,8 +102,7 @@ public class Main extends JFrame {
 			}
 		});
 		
-		
-		
+		// When the user clicks on the screen, the animation stops
 		addMouseListener(new MouseAdapter(){
 
 			@Override
@@ -106,8 +111,7 @@ public class Main extends JFrame {
 			}		
 		});
 		
-		
-		
+		// Timer to keep the animation going
 		ActionListener updateTask = new ActionListener(){
 
 			@Override
@@ -126,10 +130,11 @@ public class Main extends JFrame {
 			
 		};
 		
+		// Event is triggered every 10ms
 		new Timer(10,updateTask).start();
 	}
 	
-	
+	// Main call to the frame
 	public static void main(String[] args){
 		SwingUtilities.invokeLater(new Runnable(){
 
